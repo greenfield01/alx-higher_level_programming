c6f6e5cdc894a556773c82d22a5a7c2aab364a1b
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2022-12-20 00:23:32 +0100

    test

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+# 102-magic_calculation.py
+
+
 def magic_calculation(a, b):
     result = 0
-    return result
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except:
+            result = b + a
+            break
+    return (result)
