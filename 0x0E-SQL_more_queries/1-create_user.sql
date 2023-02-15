c2307d677fbef17f054983cce13767ffe029840c
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2023-02-16 00:02:01 +0100

    more sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,2 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+-- Creates the database hbtn_0c_0.
+CREATE DATABASE IF NOT EXISTS `user_0d_1`;
