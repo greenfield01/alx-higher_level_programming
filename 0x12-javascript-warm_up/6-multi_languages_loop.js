ddb070298c96a6d7abd58835b0629b239b167ea2
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2023-03-14 09:39:07 +0100

    103

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,16 +1,5 @@
 #!/usr/bin/node
-
-let list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0; i < list.length; i++) {
-	console.log(list[i]);
+const sentences = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < sentences.length; i++) {
+  console.log(sentences[i]);
 }
-
-/*
-
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
-}
-
-*/
-
