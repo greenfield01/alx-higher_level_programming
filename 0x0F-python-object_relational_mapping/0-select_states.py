060c172627f314d84afe9c02b0cd33f158ab2b1c
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2023-03-18 18:09:51 +0100

    relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,6 @@
 import sys
 import MySQLdb
 
-print('{},{},{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
