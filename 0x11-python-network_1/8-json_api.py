b2cffdf6f7df19d05c2ef73ec2ac031c72c7247c
Greenfield Adolphus <greenfieldadolphus22@gmail.com>
2023-04-03 00:43:36 +0100

    test

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,21 @@
-
 #!/usr/bin/python3
-"""
-Script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
 
-sage: ./8-json_api.py <letter>
+Usage: ./8-json_api.py <letter>
   - The letter is sent as the value of the variable `q`.
   - If no letter is provided, sends `q=""`.
 """
-from sys import argv
+import sys
 import requests
 
 
 if __name__ == "__main__":
-    letter = "" if len(argv) == 1 else argv[1]
-    req = requests.post("http://0.0.0.0:5000/search_user", {"q": letter})
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = req.json()
+        response = r.json()
         if response == {}:
             print("No result")
         else:
